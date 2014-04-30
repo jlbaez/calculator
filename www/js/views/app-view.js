1b5eb13d46d04c580c8e53e773b7803e92d845cd
brokenlizard56 <jlb406@gmail.com>
2014-04-30 18:09:08 -0400

    Revert "keypad to right user input added"
    This reverts commit e521de46252eb4b5cebe11d0ca719ec472f684ff.

--- www/js/views/app-view.js
+++ www/js/views/app-view.js
@@ -30,7 +30,6 @@ var decimal = false,
 		},
 		addInput: function(e){
 			var array = ['*', '/', '+'],
-                       
 				functions = ['sin', 'cos', 'tan', 'ln', 'log'],
 				value = e.currentTarget.value,
 				lastchar = this.$input.value.charAt(this.$input.value.length - 1);
@@ -107,50 +106,5 @@ var decimal = false,
 			e.preventDefault();
 			console.log("here");
 		}
-
-
-
-
 	});
-
-// FROM INPUT BOX TO MEM
-    $("div#Button button#Button_btnToMem").click(function () {
-        $(Button_Mem).val($(Button_UserInput).val());
-        $(Button_UserInput).val(strEmpty);
-        $(Button_UserInput).focus();
-    });
-
-    // FROM MEM TO INPUT BOX 
-    $("div#Button button#Button_btnFromMem").click(function () {
-        $(Button_UserInput).val($(Button_UserInput).val() + $(Button_Mem).val());
-        $(Button_Mem).val(strEmpty);
-        $(Button_UserInput).focus();
-    });
-
-
-
-// FUNCTION KEYS' EVENT HANDLER
-    $("button.Button_btnCommand").click(function () {
-        var inputBox = $(Button_UserInput);
-        var x = parseFloat(inputBox.val());
-        var retVal = "ERROR";
-
-        switch (this.id) {
-            case 'Button_btnInverseSign': retVal = -x; break;       // +/-
-            case 'Button_btnInverse': retVal = 1 / x; break;        // 1/X
-            case 'Button_btnSquare': retVal = x * x; break;         // X^2
-            case 'Button_btnSquareRoot': retVal = Math.sqrt(x); break;  // SQRT(X)
-            case 'Button^': retVal = x * x * x; break;       // X^3
-        
-            case 'Buttonln': retVal = Math.log(x); break;      // LOG (N) - NATURAL
-            case 'Button_btnExp': retVal = Math.exp(x); break;      // E^(X)
-            case 'Buttonsin': retVal = Math.sin(x); break;      // SIN(X)
-            case 'Buttoncos': retVal = Math.cos(x); break;    // COS(X) 
-            case 'Button.tan': retVal = Math.tan(x); break;       // TAN(X)
-            default: break;
-        }
-        inputBox.val(retVal);
-        inputBox.focus();
-    });
-
 })(jQuery);
